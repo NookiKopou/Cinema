a19f6e18661a135488d4a1baf7f3deabdbfdb6f1
nooki.0103@gmail.com <nooki.0103@gmail.com>
2022-10-31 14:01:55 +0700

    Созданы модели и интерфейсы

---
+++ src/Cinema/Data/Models/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinema.Data.Models
+{
+    public class Genre
+    {
+        public int id { set; get; }
+
+        public string genreName { set; get; }
+
+        public List<Movie> movies { set; get; }
+    }
+}
